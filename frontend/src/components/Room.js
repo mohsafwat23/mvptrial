e0e36a66609590497d8216bfe1dc0c72a4d50e4a
mohsafwat23 <safwat@wisc.edu>
2021-08-08 20:36:07 +0200

    added id

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -147,7 +147,7 @@ export default class Room extends Component {
             <div>
                <div className='cardContainer'>
                 {characters.map((character)=>
-                    <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+                    <TinderCard className='swipe' key={character.id} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
                         <div style={{ backgroundImage: 'url(' + character.image + ')' }} className='card'>
                         <h3>{character.name}</h3>
                         </div>
