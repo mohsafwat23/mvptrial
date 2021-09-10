2e9b3fdd9d2a899eac6a2b52280e50e2bb8d1471
David <khachatryandavid1@gmail.com>
2021-09-09 19:03:46 -0500

    mime issues solved

--- api/views.py
+++ api/views.py
@@ -217,6 +217,18 @@ def GetSwipe(request):
     return JsonResponse({"roomCode" : "fhg"})
 
 
+# Solves the MIME issue
+class Assets(View):
+
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), content_type='application/javascript')
+        else:
+            return HttpResponseNotFound()
+
 @csrf_exempt
 def CheckMatch(request):
 
